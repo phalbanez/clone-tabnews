bdca7ecad491934d4635d918a13df9034ad6ae55
Paulo H Albanez <phalbanez@gmail.com>
2026-01-26 16:24:33 -0300

    feat: allow `update:user:others` to update other users

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -98,6 +98,11 @@ function extractUUID(text) {
   return match ? match[0] : null;
 }
 
+async function addFeaturesToUser(userObject, features) {
+  const updatedUser = await user.addFeatures(userObject.id, features);
+  return updatedUser;
+}
+
 const orchestrator = {
   waitForAllServices,
   clearDatabase,
@@ -108,6 +113,7 @@ const orchestrator = {
   deleteAllEmails,
   getLastEmail,
   extractUUID,
+  addFeaturesToUser,
 };
 
 export default orchestrator;
